6560845c6d5b4d6de8843bdc9352340169100683
Olivier Adam <olivieradam666+spam@gmail.com>
2015-09-19 00:49:54 +0200

    gomux not working

--- main.go
+++ main.go
@@ -6,26 +6,48 @@ import (
 	"net/http"
 )
 
-var router = mux.NewRouter()
-var authRouter = router.PathPrefix("/authenticated-resources").Subrouter()
-var unAuthRouter = router.PathPrefix("/unauthenticated-resources").Subrouter()
+const (
+	authPrefix   = "/authenticated-resources/"
+	unAuthPrefix = "/unauthenticated-resources/"
+)
+
+var (
+	unAuthRouter = mux.NewRouter()
+	authRouter   = mux.NewRouter()
+)
 
 func init() {
-	http.Handle("/", router)
+	http.Handle(unAuthPrefix, http.StripPrefix(unAuthPrefix, unAuthRouter))
+	http.Handle(authPrefix, addAuthCheck(http.StripPrefix(authPrefix, authRouter)))
 	//auth
 	authRouter.HandleFunc("/toto", wakeUp)
+	authRouter.HandleFunc("/{yo:.*}", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, r.URL)
+	})
 	//unauth
 	unAuthRouter.HandleFunc("/wakeUp", wakeUp)
+	unAuthRouter.HandleFunc("/", wakeUp)
+	unAuthRouter.HandleFunc("/{yo:.*}", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, r.URL)
+	})
 	//unAuthRouter.HandleFunc("/party", createParty).Methods("POST")
 }
 
-/*func checkAuth(h http.HandlerFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		//var partyId = request.Header.Get("dedguenodgo-partyId")
-		//var partyPassword = request.Header.Get("dedguenodgo-partyPassword")
-		h(w, r)
-	}
-}*/
+func addAuthCheck(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		var partyId = r.Header.Get("dedguenodgo-partyId")
+		var partyPassword = r.Header.Get("dedguenodgo-partyPassword")
+		if !checkAuth(partyId, partyPassword) {
+			http.Error(w, "wrong username/password", http.StatusUnauthorized)
+		} else {
+			h.ServeHTTP(w, r)
+		}
+	})
+}
+
+func checkAuth(partyId, partyPassword string) bool {
+	return partyId != "partyId" || partyPassword != "partyPassword"
+}
 
 func wakeUp(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "OK")
