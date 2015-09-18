16b9ae931feeff5a442bbc4b09f48b090b10bc3f
Olivier Adam <olivieradam666+spam@gmail.com>
2015-09-19 01:24:01 +0200

    Admin password check works fine

--- main.go
+++ main.go
@@ -1,36 +1,23 @@
 package nonoel
 
 import (
+	"appengine"
 	"fmt"
-	"github.com/gorilla/mux"
 	"net/http"
 )
 
-const (
-	authPrefix   = "/authenticated-resources/"
-	unAuthPrefix = "/unauthenticated-resources/"
-)
-
-var (
-	unAuthRouter = mux.NewRouter()
-	authRouter   = mux.NewRouter()
-)
-
 func init() {
-	http.Handle(unAuthPrefix, http.StripPrefix(unAuthPrefix, unAuthRouter))
-	http.Handle(authPrefix, addAuthCheck(http.StripPrefix(authPrefix, authRouter)))
-	//auth
-	authRouter.HandleFunc("/toto", wakeUp)
-	authRouter.HandleFunc("/{yo:.*}", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, r.URL)
-	})
-	//unauth
-	unAuthRouter.HandleFunc("/wakeUp", wakeUp)
-	unAuthRouter.HandleFunc("/", wakeUp)
-	unAuthRouter.HandleFunc("/{yo:.*}", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, r.URL)
-	})
-	//unAuthRouter.HandleFunc("/party", createParty).Methods("POST")
+	addUnauth("/wakeUp", noop)
+	addUnauth("/party", createParty)
+	addAuth("/hello", noop)
+}
+
+func addUnauth(subpath string, f func(w http.ResponseWriter, r *http.Request)) {
+	http.HandleFunc("/unauthenticated-resources"+subpath, f)
+}
+
+func addAuth(subpath string, f func(w http.ResponseWriter, r *http.Request)) {
+	http.Handle("/authenticated-resources"+subpath, addAuthCheck(http.HandlerFunc(f)))
 }
 
 func addAuthCheck(h http.Handler) http.Handler {
@@ -46,17 +33,24 @@ func addAuthCheck(h http.Handler) http.Handler {
 }
 
 func checkAuth(partyId, partyPassword string) bool {
-	return partyId != "partyId" || partyPassword != "partyPassword"
+	return partyId == "partyId" && partyPassword == "partyPassword"
 }
 
-func wakeUp(w http.ResponseWriter, r *http.Request) {
+func noop(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "OK")
 }
 
-/*func createParty(w http.ResponseWriter, r *http.Request) {
+func createParty(w http.ResponseWriter, r *http.Request) {
+	c := appengine.NewContext(r)
 	adminPassword := r.FormValue("adminPassword")
+	err := CheckOrSetAdminPassword(c, adminPassword)
+	if err != nil {
+		http.Error(w, "wrong admin password", http.StatusUnauthorized)
+		return
+	}
+
 	partyName := r.FormValue("partyName")
 	partyPassword := r.FormValue("partyPassword")
 
-	fmt.Fprintf(w, "Party %s created successfully", partyName)
-}*/
+	fmt.Fprintf(w, "Party %s/%s created successfully", partyName, partyPassword)
+}
